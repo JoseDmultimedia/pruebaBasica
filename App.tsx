1905b77fdf86ff92b680cbdee1d0caaa5459a7b2
JoseDmultimedia <garciadiosaj@gmail.com>
2023-06-28 21:18:12 -0500

    core: added navigation packages

--- App.tsx
+++ App.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {StyleSheet, Text, View} from 'react-native';
 import TextForm from './src/components/TextForm/TextForm';
 import Saludar from './src/components/Saludar/Saludar';
 
 function App(): JSX.Element {
   return (
-    <View style={styles.container}>
-      <Text>Hola mundo esta es una app</Text>
-      <TextForm variable='1' varible2={2}/>
-      <Saludar name={"jose dav"} age={3}/>
-      <Saludar/>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Hola mundo esta es una app</Text>
+        <TextForm variable="1" varible2={2} />
+        <Saludar name={'jose dav'} age={3} />
+        <Saludar />
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
     // backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     // color: "black"
-  }
+  },
 });
-
-
 
 export default App;
